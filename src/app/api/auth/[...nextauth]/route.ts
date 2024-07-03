0a7b27e6eec8d6009bb2c0872bb6dba785d590f3
duyzuy <vutruongduy2109@gmail.com>
2024-07-03 14:04:57 +0700

    update

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD:src/app/api/auth/[...nextauth]/route.ts
-import NextAuth from "next-auth";
-=======
 import NextAuth from "next-auth/next";
 
->>>>>>> parent of ba05b49 (fix nextauth):src/app/api/auth/[...nextAuth]/route.ts
 import { authOptions } from "@/auth";
 const handler = NextAuth(authOptions);
 
