2708b11061219ce00127e484f0e27c9db806d035
duyzuy <vutruongduy2109@gmail.com>
2024-07-03 13:14:22 +0700

    update

--- src/app/api/auth/[...nextAuth]/route.ts
+++
@@ -1,6 +0,0 @@
-// import NextAuth from "next-auth";
-import nextAuth from "next-auth";
-import { authOptions } from "@/auth";
-
-const handler = nextAuth(authOptions);
-export { handler as GET, handler as POST };
