652ff57e024cbdda59a95a56381b29ad018b965a
duyzuy <vutruongduy2109@gmail.com>
2025-02-18 00:48:03 +0700

    update

--- src/context/ThemeModeContext.tsx
+++ src/context/ThemeModeContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, Dispatch, SetStateAction, useContext, useEffect, useState } from "react";
 import { usePersistState } from "@/hooks/usePersistState";
-import { getThemeMode, isSSR } from "@/utils/common";
+import { getThemeMode, setThemeMode } from "@/utils/common";
 
 export const ThemeModeContext = createContext<["light" | "dark", Dispatch<SetStateAction<"light" | "dark">>]>([
   "light",
@@ -8,18 +8,18 @@ export const ThemeModeContext = createContext<["light" | "dark", Dispatch<SetSta
 ]);
 
 export function ThemeModeProvider({ children }: { children: React.ReactNode }) {
-  const [themeMode, setThemeMode] = useState<"light" | "dark">(() => {
+  const [theme, setTheme] = useState<"light" | "dark">(() => {
     const themeMode = getThemeMode();
     return themeMode ?? "light";
   });
 
   useEffect(() => {
-    setThemeMode(themeMode);
-  }, [themeMode]);
+    setThemeMode(theme);
+  }, [theme]);
 
   // const [themeMode, setThemeMode] = usePersistState<"light" | "dark">({ key: "theme-mode", initialValue: "light" });
 
-  return <ThemeModeContext.Provider value={[themeMode, setThemeMode]}>{children}</ThemeModeContext.Provider>;
+  return <ThemeModeContext.Provider value={[theme, setTheme]}>{children}</ThemeModeContext.Provider>;
 }
 
 export function useThemeMode() {
